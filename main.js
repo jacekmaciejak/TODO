986d757ab4799e1aec63b315285283a16e7726ef
Jacek Maciejak <jacekmaciejak@poczta.fm>
2020-04-14 20:11:01 +0200

    New design

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const LOCAL_STORAGE_LIST_KEY = 'task.lists'
-const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+const LOCAL_STORAGE_LIST_KEY = "task.lists";
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = "task.selectedListId";
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
-let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
 let toDoList = [];
 const listsContainer = document.querySelector("[data-lists]");
 const newListForm = document.querySelector("[data-new-list-form]");
 const newListInput = document.querySelector("[data-new-list-input]");
-const deleteListButton = document.querySelector('[data-delete-list-button]')
+const deleteListButton = document.querySelector("[data-delete-list-button]");
 
-const listDisplayContainer = document.querySelector('[data-list-display-container]')
-const listTitleElement = document.querySelector('[data-list-title]')
+const listDisplayContainer = document.querySelector(
+  "[data-list-display-container]"
+);
+const listTitleElement = document.querySelector("[data-list-title]");
 
 const inputSearch = document.querySelector(".search");
 const inputAdd = document.querySelector(".addTask");
@@ -20,70 +22,70 @@ let taskNumber = document.querySelector("h1 span");
 const clock = document.createElement("div");
 clock.className = "clock";
 
-//Selecting which ID we selected
-listsContainer.addEventListener('click', e => {
-  if (e.target.tagName.toLowerCase() === 'li') {
+//Selecting which ID was selected
+listsContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "li") {
     selectedListId = e.target.dataset.listId;
-    saveAndRender()
+    saveAndRender();
   }
-})
+});
 //Deleted active/selected list
-deleteListButton.addEventListener('click', e => {
-  lists = lists.filter(list => list.id !== selectedListId)
+deleteListButton.addEventListener("click", (e) => {
+  lists = lists.filter((list) => list.id !== selectedListId);
   selectedListId = null;
-  saveAndRender()
-})
+  saveAndRender();
+});
 
-newListForm.addEventListener("submit", e => {
+newListForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const listName = newListInput.value;
   if (listName == null || listName === "") return;
   const list = createList(listName);
   newListInput.value = null;
   lists.push(list);
-  saveAndRender()
+  saveAndRender();
 });
 
 function createList(name) {
   return {
     id: Date.now().toString(),
     name: name,
-    tasks: []
+    tasks: [],
   };
 }
 //Only functions call
 function saveAndRender() {
-  save()
-  render()
+  save();
+  render();
 }
 //Function which save lists to loacalstorage
 function save() {
-  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists)) //saving lists
-  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId) //saving selected list ID
+  localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists)); //saving lists
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId); //saving selected list ID
 }
 
 function render() {
   clearElement(listsContainer);
-  renderLists()
+  renderLists();
 
-  const selectedList = lists.find(list => list.id === selectedListId)
+  const selectedList = lists.find((list) => list.id === selectedListId);
   //if list is activ hide or show tasks list
   if (selectedListId == null) {
-    listDisplayContainer.style.display = 'none'
+    listDisplayContainer.style.display = "none";
   } else {
-    listDisplayContainer.style.display = ''
+    listDisplayContainer.style.display = "";
     listTitleElement.textContent = selectedList.name;
   }
 }
 
 function renderLists() {
-  lists.forEach(list => {
+  lists.forEach((list) => {
     const listElement = document.createElement("li");
     listElement.dataset.listId = list.id;
     listElement.classList.add("list-name");
     listElement.innerText = list.name;
     if (list.id === selectedListId) {
-      listElement.classList.add('active-list')
+      listElement.classList.add("active-list");
     }
     listsContainer.appendChild(listElement);
   });
@@ -103,16 +105,16 @@ render();
 // }
 // abc()
 
-const searchTask = e => {
+const searchTask = (e) => {
   const searchText = e.target.value.toLowerCase();
   let tasks = [...liElements];
-  tasks = toDoList.filter(li =>
+  tasks = toDoList.filter((li) =>
     li.textContent.toLowerCase().includes(searchText)
   );
   wrapper.textContent = "";
-  tasks.forEach(li => wrapper.appendChild(li));
+  tasks.forEach((li) => wrapper.appendChild(li));
 };
-const removeNewTask = e => {
+const removeNewTask = (e) => {
   e.target.parentNode.remove();
   const index = e.target.parentNode.dataset.key;
   toDoList.splice(index, 1);
@@ -120,12 +122,22 @@ const removeNewTask = e => {
   // renderList();
 };
 
-const addTask = e => {
+//new------------------
+function createTask(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    tasks: [],
+  };
+}
+
+const addTask = (e) => {
   e.preventDefault();
   const taskBox = document.createElement("div");
   taskBox.className = "taskBox";
   const titleTask = inputAdd.value;
-  const task = document.createElement("textarea");
+  // const task = document.createElement("textarea");
+  const task = createTask(titleTask); //new--------------
   task.className = "task";
   task.disabled = true;
   task.type = "text";
@@ -144,10 +156,13 @@ const addTask = e => {
   editBtn.appendChild(document.createTextNode("EDIT"));
   task.innerHTML = titleTask;
   toDoList.push(task);
-  taskBox.appendChild(task);
+  taskBox.append(task);
   taskBox.appendChild(editBtn);
   taskBox.appendChild(deleteBtn);
   wrapper.appendChild(taskBox);
+  const selectedList = lists.find((list) => list.id === selectedListId); //new
+  selectedList.tasks.push(task); //new
+  saveAndRender(); //new
   addBackground();
   inputAdd.value = "";
   taskNumber.textContent = liElements.length;
@@ -167,7 +182,7 @@ const renderList = () => {
 
 //Zmiana kolor t?a dla nieparzystych
 const addBackground = () => {
-  const odd = document.querySelectorAll("div:nth-child(odd)"); // zamiast div bylo li
+  const odd = document.querySelectorAll("wrapper div:nth-child(odd)"); // zamiast div bylo li
   for (let i = 0; i < odd.length; i++) {
     odd[i].style.backgroundColor = "#f4f4f4";
   }
@@ -183,9 +198,9 @@ function timeSet() {
   const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
   const day = time.getDate() < 10 ? "0" + time.getDate() : time.getDate();
   const month =
-    time.getMonth() + 1 < 10 ?
-    "0" + (time.getMonth() + 1) :
-    time.getMonth() + 1;
+    time.getMonth() + 1 < 10
+      ? "0" + (time.getMonth() + 1)
+      : time.getMonth() + 1;
   const year =
     time.getFullYear() < 10 ? "0" + time.getFullYear() : time.getFullYear();
   clock.innerHTML = `${hours} : ${minutes} : ${seconds} || ${day} - ${month} - ${year} r.`;
